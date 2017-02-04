a798aee48a725bfcfdf27661439e7325d5caaf6d
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-04 20:05:44 +0200

    Animate new texts

--- src/components/AnimatedButton.js
+++ src/components/AnimatedButton.js
@@ -8,7 +8,9 @@ class AnimatedButton extends Component {
 
     this.state = {
       animatedValue: new Animated.Value(0),
+      textAnimation: new Animated.Value(0),
       oldColor: this.getVariantColor(props.variant),
+      oldText: props.children,
     }
   }
 
@@ -16,13 +18,20 @@ class AnimatedButton extends Component {
     if (this.props.variant !== nextProps.variant) {
       this.setState({oldColor: this.getVariantColor(this.props.variant)});
     }
+    if (this.props.children !== nextProps.children) {
+      this.setState({oldText: this.props.children});
+    }
   }
   
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.variant !== this.props.variant) {
       this.state.animatedValue.setValue(0);
-      Animated.timing(this.state.animatedValue, {toValue: 1}).start();
+      Animated.timing(this.state.animatedValue, {toValue: 1, duration: 200}).start();
+    }
+    if (prevProps.children !== this.props.children) {
+      this.state.textAnimation.setValue(0);
+      Animated.timing(this.state.textAnimation, {toValue: 1, duration: 300}).start();
     }
   }
 
@@ -44,10 +53,23 @@ class AnimatedButton extends Component {
       inputRange: [0, 1],
       outputRange: [this.state.oldColor, this.getVariantColor(this.props.variant)],
     });
+    const textPosition = this.state.textAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 40],
+    });
+    const hiddenTextPosition = this.state.textAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-35, 15],
+    });
 
     return (
       <Animated.View style={[styles.container, {backgroundColor: buttonBackgroundColor}]}>
-        <Text style={styles.text}>{this.props.children.toUpperCase()}</Text>
+        <Animated.Text style={[styles.hiddenText, {top: hiddenTextPosition}]}>
+          {this.props.children.toUpperCase()}
+        </Animated.Text>
+        <Animated.Text style={[styles.text, {transform: [{translateY: textPosition}]}]}>
+          {this.state.oldText.toUpperCase()}
+        </Animated.Text>
       </Animated.View>
     );
   }
@@ -66,10 +88,20 @@ const styles = StyleSheet.create({
     padding: 15,
     width: 200,
     borderRadius: 40,
+    overflow: 'hidden',
   },
   text: {
     color: '#ffffff',
     fontSize: 24,
+    textAlign: 'center',
+  },
+  hiddenText: {
+    color: '#ffffff',
+    fontSize: 24,
+    position: 'absolute',
+    top: -35,
+    left: 0,
+    right: 0,
     textAlign: 'center',
   }
 });

130f796d3a6dfb1d3a3c882b35f0d685e4e94db5
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-04 20:10:36 +0200

    Change two button props at the same time

--- src/components/App.js
+++ src/components/App.js
@@ -9,29 +9,15 @@ class App extends Component {
     text: 'Submit',
   }
   
-  changeVariant = buttonVariant => {
-    this.setState({buttonVariant});
-  }
-
-  changeText = text => {
-    this.setState({text});
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>Variants</Text>
-        <View style={styles.variants}>
-          <Button title="Error" onPress={() => this.changeVariant('error')} />
-          <Button title="Primary" onPress={() => this.changeVariant('primary')} />
-          <Button title="Success" onPress={() => this.changeVariant('success')} />
-        </View>
-        <Text style={styles.header}>Variants</Text>
+        <Text style={styles.header}>Change button state</Text>
         <View style={styles.texts}>
-          <Button title="Submit" onPress={() => this.changeText('Submit')} />
-          <Button title="Whoops" onPress={() => this.changeText('Whoops')} />
-          <Button title="Retry" onPress={() => this.changeText('Retry')} />
-          <Button title="Thanks" onPress={() => this.changeText('Thanks')} />
+          <Button title="Submit" onPress={() => this.setState({text: 'Submit', buttonVariant: 'primary'})} />
+          <Button title="Whoops" onPress={() => this.setState({text: 'Whoops', buttonVariant: 'error'})} />
+          <Button title="Retry" onPress={() => this.setState({text: 'Retry', buttonVariant: 'error'})} />
+          <Button title="Thanks" onPress={() => this.setState({text: 'Thanks', buttonVariant: 'success'})} />
         </View>
         <AnimatedButton variant={this.state.buttonVariant}>
           {this.state.text}
@@ -52,9 +38,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  variants: {
-    flexDirection: 'row',
   },
   texts: {
     flexDirection: 'row',
